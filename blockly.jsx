de535f344d554a72416a3e722c62ecf37c196f9f
Tim Kay <timkay@not.com>
2024-09-29 09:23:24 -0700

    recursive works, adding test cases

--- blockly.jsx
+++ blockly.jsx
@@ -10,120 +10,238 @@ const rulemap = {
     amount: 0,
 }
 
-function gen_options(options) {
+function options(options) {
     return options.split(' ').map(key => key.startsWith('-')
         ? <option key={key} disabled>{key}</option>
         : <option key={key}>{key}</option>)
 }
 
-function Between({rule}) {
-    const {render} = useContext(AuthorizationContext)
-    const valueHandler = (event, i, blur) => {
-        let value = event.target.value
-        if (blur) value = parseFloat(value).toFixed(2)
-        rule.params[i] = value
-        render()
+function handler(event, item, index, render) {
+    if (index === 'metric') {
+        item.op = ''
+        item.params = []
     }
+    item[index] = event.target.value
+    render()
+}
+
+function dollarHandler(event, item, index, render) {
+    let value = event.target.value
+    if (event.type === 'blur' || event.type === 'keyup' && event.key === 'Enter') {
+        if (value.match(/^[\-\.\d]+$/)) {
+            value = parseFloat(value).toFixed(2)
+        }
+    }
+    item[index] = value
+    render()
+}
+
+function Text({item, index = 0, type = 'text'}) {
+    const {render} = useContext(AuthorizationContext)
+    return <input className="text" type={type} value={item[index]}
+        onFocus={event => event.target.select()}
+        onChange={event => handler(event, item, index, render)}
+        onBlur={event => handler(event, item, index, render)}
+        onKeyUp={event => handler(event, item, index, render)}
+    />
+}
+
+
+function Expr({item, index = 0}) {
+    const {render} = useContext(AuthorizationContext)
+    return <input className="number" type="text" value={item[index]}
+        onFocus={event => event.target.select()}
+        onChange={event => dollarHandler(event, item, index, render)}
+        onBlur={event => dollarHandler(event, item, index, render)}
+        onKeyUp={event => dollarHandler(event, item, index, render)}
+    />
+}
+
+function Between({rule}) {
     rule.params ??= ['0.00', '0.00']
     return <div>
-        <input type="number" value={rule.params[0]}
-            onChange={event => valueHandler(event, 0)}
-            onBlur={event => valueHandler(event, 0, true)}
-            onKeyUp={event => valueHandler(event, 0, event.key === 'Enter')}
-        />
+        <Expr item={rule.params} index={0} />
         &nbsp;and&nbsp;
-        <input type="number" value={rule.params[1]}
-            onChange={event => valueHandler(event, 1)}
-            onBlur={event => valueHandler(event, 1, true)}
-            onKeyUp={event => valueHandler(event, 1, event.key === 'Enter')}
-        />
+        <Expr item={rule.params} index={1} />
     </div>
 }
 
-function Comparison({rule, op}) {
-    const {render} = useContext(AuthorizationContext)
-    const valueHandler = (event, i, blur) => {
-        let value = event.target.value
-        if (blur) value = parseFloat(value).toFixed(2)
-        rule.params[i] = value
-        render()
-    }
+function Compare({rule, op}) {
     rule.params ??= ['0.00']
     return <div>
-        <input type="number" value={rule.params[0]}
-            onChange={event => valueHandler(event, 0)}
-            onBlur={event => valueHandler(event, 0, true)}
-            onKeyUp={event => valueHandler(event, 0, event.key === 'Enter')}
-        />
+        <Expr item={rule.params} index={0} />
     </div>
 }
 
-function Amount({rule}) {
+function AmountRule({rule}) {
     const {render} = useContext(AuthorizationContext)
-    const valueHandler = event => {
-        rule.op = event.target.value
-        render()
+    if (!rule.op || ! rule.params) {
+        rule.op = 'between'
+        rule.params = ['0.00', '0.00']
     }
     return <div>
         is&nbsp;
-        <select value={rule.op} onChange={valueHandler}>
-            {gen_options('between abc < = >')}
+        <select value={rule.op} onChange={event => handler(event, rule, 'op', render)}>
+            {options('between < <= = >= >')}
         </select>
         {rule.op === 'between' && <Between rule={rule} />}
-        {rule.op === '<' && <Comparison rule={rule} />}
-        {rule.op === '=' && <Comparison rule={rule} />}
-        {rule.op === '>' && <Comparison rule={rule} />}
+        {rule.op === '<' && <Compare rule={rule} />}
+        {rule.op === '<=' && <Compare rule={rule} />}
+        {rule.op === '=' && <Compare rule={rule} />}
+        {rule.op === '>=' && <Compare rule={rule} />}
+        {rule.op === '>' && <Compare rule={rule} />}
     </div>
 }
 
-function Mcc({rule}) {
-    return ''
+function MccRule({rule}) {
+    const {render} = useContext(AuthorizationContext)
+    if (!rule.op || !rule.params) {
+        rule.op ??= 'allow'
+        rule.params ??= ['']
+        // render()
+    }
+    return <div>
+        is&nbsp;
+        <select value={rule.op} onChange={event => handler(event, rule, 'op', render)}>
+            {options('allow disallow')}
+        </select>
+        <div>
+            <Text item={rule.params} index={0} />
+        </div>
+    </div>
+}
+
+function DescriptionRule({rule}) {
+    const {render} = useContext(AuthorizationContext)
+    if (!rule.op || !rule.params) {
+        rule.op ??= 'contains'
+        rule.params ??= ['']
+        // render()
+    }
+    return <div>
+        <select value={rule.op} onChange={event => handler(event, rule, 'op', render)}>
+            {options('contains does-not-contain')}
+        </select>
+        <div>
+            <Text item={rule.params} index={0} />
+        </div>
+    </div>
+}
+
+function DateRule({rule, type = 'date'}) {
+    const {render} = useContext(AuthorizationContext)
+    if (!rule.op) {
+        rule.op ||= 'between'
+        // render()
+    }
+    return <div>
+        <select value={rule.op} onChange={event => handler(event, rule, 'op', render)}>
+            {options('between after before')}
+        </select>
+        {rule.op === 'between'
+        && <div>
+            <Text type={type} item={rule.params} index={0} />
+            &nbsp;and&nbsp;
+            <Text type={type} item={rule.params} index={1} />
+        </div>
+        || <div>
+            <Text type={type} item={rule.params} index={0} />
+        </div>
+        }
+    </div>
+}
+
+function Aggregate({rule}) {
+    if (!rule.rules || rule.rules.length === 0) {
+        rule.rules = [{}]
+    }
+    return <>
+        <br/>
+        <Rules rules={rule.rules} />
+    </>
 }
 
 function Rule({rule}) {
-    const op = rule.op
     const {render} = useContext(AuthorizationContext)
-    const valueHandler = event => {
-        rule.metric = event.target.value
-        render()
-    }
-    return <div>
-        <select value={rule.metric} onChange={valueHandler}>
-            {gen_options('Choose: amount balance datetime date time mcc description -------------------- Any All None')}
+    return <>
+        <select value={rule.metric} onChange={event => handler(event, rule, 'metric', render)}>
+            {options('Choose: amount balance date time datetime mcc mid description --------------- All Any None')}
         </select>
-        {rule.metric === 'amount' && <Amount rule={rule} />}
-        {rule.metric === 'mcc' && <Mcc rule={rule} />}
-    </div>
+        {rule.metric === 'amount'      && <AmountRule rule={rule} />}
+        {rule.metric === 'balance'     && <AmountRule rule={rule} />}
+        {rule.metric === 'date'        && <DateRule rule={rule} />}
+        {rule.metric === 'datetime'    && <DateRule rule={rule} type="datetime-local" />}
+        {rule.metric === 'description' && <DescriptionRule rule={rule} />}
+        {rule.metric === 'mcc'         && <MccRule rule={rule} />}
+        {rule.metric === 'mid'         && <MccRule rule={rule} />}
+        {rule.metric === 'time'        && <DateRule rule={rule} type="time" />}
+        {rule.metric === 'All'         && <Aggregate rule={rule} />}
+        {rule.metric === 'Any'         && <Aggregate rule={rule} />}
+        {rule.metric === 'None'        && <Aggregate rule={rule} />}
+    </>
 }
 
 function Rules({rules}) {
+    console.log('rules', rules)
     const {render} = useContext(AuthorizationContext)
-    const plusHandler = i => {
-        rules.splice(i + 1, 0, {key: rules.length})
+    const addHandler = i => {
+        const add = structuredClone(rules[i])
+        add.key = rules.length
+        rules.splice(i + 1, 0, add)
         render()
+    }
+    const remHandler = i => {
+        if (rules.length > 1) {
+            rules.splice(i, 1)
+            render()
+        }
     }
     rules.forEach((rule, i) => rule.key ||= i)
     return <>
-        {rules.map((rule, i) =>
-            <div key={`rule-${rule.key}`}>
-                <div className="rule">
-                    <div className="plus">⋮⋮&nbsp;</div>
-                    <Rule rule={rule} />
-                    <div className="plus" onClick={() => plusHandler(i)}>&nbsp;+</div>
-                </div>
-            </div>)}
+        {rules.map((rule, i) => <>
+            <div className="rule" key={`rule-${rule.key}`}>
+                <div title="delete rule" className="plus" onClick={() => remHandler(i)}>－</div>
+                <div title="add rule" className="plus" onClick={() => addHandler(i)}>＋</div>
+                <div className="drag">⋮⋮&nbsp;</div>
+                <Rule rule={rule} />
+            </div>
+            <br/>
+        </>)}
     </>
 }
 
 const rules_history = [
     [
-        {metric: 'amount', op: 'between', params: ['', '100.00']},
-        {metric: 'mcc',    op: 'in',      params: ['3112', '3114', '4710']},
+        {},
     ]
 ]
 
+const test_data = [
+    {
+        date: '2024-09-29 15:12:01',
+        description: 'McDonalds #4321, Lansing MI',
+        amount: 12.54,
+        mcc: 2402,
+    },
+    {
+        date: '2024-09-30 15:12:01',
+        description: 'McDonalds #4321, Lansing MI',
+        amount: 12.54,
+        mcc: 2402,
+    },
+    {
+        date: '2024-09-29 15:12:01',
+        description: 'Ronalds #1, Texas MI',
+        amount: 1200,
+        mcc: 3501,
+    },
+]
+
+const test_balance = 833.23
+
 function Authorization() {
     const [rules, setRules] = useState()
+    const [colors, setColors] = useState('')
     const render = () => {
         setRules(rules => {
             if (rules) rules_history.unshift(rules)
@@ -131,17 +249,14 @@ function Authorization() {
         })
     }
     if (!rules) {
-        console.log(rules)
         render()
-        return ''
+        return 
     }
-    console.log('(rendering) history', rules_history)
-    console.log('(rendering) rules', rules)
     return  <>
+        <pre style={{position: 'fixed', fontSize: 'x-small', top: 10, right: 10}}>{JSON.stringify(rules.map(x => (delete x.key, x)), null, 4)}</pre>
         <AuthorizationContext.Provider value={{render}}>
             <Rules rules={rules} />
         </AuthorizationContext.Provider>
-        <pre style={{position: 'fixed', top: 10, right: 10}}>{JSON.stringify(rules, null, 4)}</pre>
     </>
 }
 
