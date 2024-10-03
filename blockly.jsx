3d4aa9574d5720015377f300ae3053120e0c9d4a
Tim Kay <timkay@not.com>
2024-10-03 04:37:01 -0700

    is_authorized.js is downloadable

--- blockly.jsx
+++ blockly.jsx
@@ -242,110 +242,7 @@ const rules_history = [
 ]
 
 
-
-const dict = x => x
-const len = list => list.length
-const float = item => parseFloat(item)
-const contains = (text, pattern) => !!text.match(pattern)
-
-const __in__ = (expr, data) => data.includes(expr);
-String.prototype.lower = String.prototype.toLowerCase;
-String.prototype.py_split = function (s) {return this.split(s)}
-
-var test_data = [dict ({'datetime': '2024-09-29 15:12:01', 'description': 'McDonalds #4321, Lansing MI', 'amount': '12.54', 'balance': '833.23', 'mcc': '2402'}), dict ({'datetime': '2024-09-30 18:15:42', 'description': 'McDonalds #4321, Lansing MI', 'amount': '15.99', 'balance': '813.23', 'mcc': '2402'}), dict ({'datetime': '2024-09-30 15:12:01', 'description': 'Ronalds #1, Texas MI', 'amount': '1200.00', 'balance': '604.17', 'mcc': '3501'}), dict ({'datetime': '2024-09-30 77:19:25', 'description': 'Southwest / Dallas, TX', 'amount': '237.90', 'balance': '604.17', 'mcc': '5213'}), dict ({'datetime': '2024-10-01 15:12:01', 'description': 'Peets 1744, Ann Arbor MIUS', 'amount': '6.65', 'balance': '907.68', 'mcc': '2403'})];
-for (var data of test_data) {
-        var __left0__ = data ['datetime'].py_split (' ');
-        data ['date'] = __left0__ [0];
-        data ['time'] = __left0__ [1];
-}
-var is_authorized = function (rules, data, all) {
-        if (typeof all == 'undefined' || (all != null && all.hasOwnProperty ("__kwargtrans__"))) {;
-                var all = true;
-        };
-        if (!(rules) || !(len (rules))) {
-                return all;
-        }
-        for (var rule of rules) {
-                var term = null;
-                var metric = rule ['metric'];
-                if (!(metric) || metric == 'Choose:') {
-                        continue;
-                }
-                if (!(rule ['active'])) {
-                        continue;
-                }
-                if (metric == 'All') {
-                        var term = is_authorized (rule ['rules'], data, true);
-                }
-                else if (metric == 'Any') {
-                        var term = is_authorized (rule ['rules'], data, false);
-                }
-                else if (metric == 'None') {
-                        var term = !(is_authorized (rule ['rules'], data, true));
-                }
-                else if (metric == 'JIT') {
-                        var term = true;
-                }
-                else {
-                        var op = rule ['op'];
-                        var value = data [metric];
-                        var expr = rule ['params'] [0];
-                        var expr1 = (op == 'between' ? rule ['params'] [1] : null);
-                        if (metric == 'amount' || metric == 'balance') {
-                                var evaluate = function (expr) {
-                                        if (expr == 'balance') {
-                                                return float (data ['balance']);
-                                        }
-                                        return float (expr || '0.00');
-                                };
-                                var value = evaluate (value);
-                                var expr = evaluate (expr);
-                                var expr1 = evaluate (expr1);
-                        }
-                        if (metric == 'amount' || metric == 'balance' || metric == 'datetime' || metric == 'date' || metric == 'time') {
-                                if (op == 'between') {
-                                        var term = expr <= value && value <= expr1;
-                                }
-                                else if (op == '<') {
-                                        var term = value < expr;
-                                }
-                                else if (op == '<=') {
-                                        var term = value <= expr;
-                                }
-                                else if (op == '=') {
-                                        var term = value == expr;
-                                }
-                                else if (op == '>=') {
-                                        var term = value >= expr;
-                                }
-                                else if (op == '>') {
-                                        var term = value > expr;
-                                }
-                                else {
-                                        return false;
-                                }
-                        }
-                        else if (metric == 'mcc') {
-                                var isin = data ['mcc'] == expr;
-                                var term = op == 'is-in' && isin || op == 'is-not-in' && !(isin);
-                        }
-                        else if (metric == 'description') {
-                                var cont = __in__ (expr.lower (), data.description.lower ());
-                                var term = op == 'contains' && cont || op == 'does-not-contain' && !(cont);
-                        }
-                        else {
-                                return false;
-                        }
-                }
-                if (all == true && term == false) {
-                        return false;
-                }
-                if (all == false && term == true) {
-                        return true;
-                }
-        }
-        return all;
-};
+let test_data, is_authorized
 
 function copyToClipboard(text) {
     navigator.clipboard.writeText(text + '\n')
@@ -408,9 +305,26 @@ function App() {
     </>
 }
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App/>)
+fetch('is_authorized.js')
+.then(res => res.text())
+.then(js => {
+    [test_data, is_authorized] = eval(`
+const dict = x => x
+const len = list => list.length
+const float = item => parseFloat(item)
+const contains = (text, pattern) => !!text.match(pattern)
+
+const __in__ = (expr, data) => data.includes(expr);
+String.prototype.lower = String.prototype.toLowerCase;
+String.prototype.py_split = function (s) {return this.split(s)}
+` + js + '; [test_data, is_authorized]')
+})
+.then(() => {
+    console.log('READY!')
+    const container = document.getElementById('root')
+    const root = createRoot(container)
+    root.render(<App/>)
+})
 
 
 
